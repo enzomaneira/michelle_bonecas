9cd0ac3a08c7b724e29fb412de2ffc77d27c0ab1
enzomaneira <enzo@maneira.com>
2024-02-28 10:41:41 -0300

    creating simple query for get posts

--- src/main/java/com/maneira/mongoproject/demo/resources/PostResource.java
+++ src/main/java/com/maneira/mongoproject/demo/resources/PostResource.java
@@ -1,15 +1,14 @@
 package com.maneira.mongoproject.demo.resources;
 
 import com.maneira.mongoproject.demo.domain.Post;
+import com.maneira.mongoproject.demo.resources.util.URL;
 import com.maneira.mongoproject.demo.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -22,6 +21,14 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+
+    @RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
