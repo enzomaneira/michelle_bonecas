eebe4256fde4736e07f2adbde2b97c1d31b1a4c1
enzomaneira <enzo@maneira.com>
2024-02-27 22:36:12 -0300

    creating service and resource for post and creating comment obj

--- src/main/java/com/maneira/mongoproject/demo/config/Instantiation.java
+++ src/main/java/com/maneira/mongoproject/demo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.maneira.mongoproject.demo.config;
 
 import com.maneira.mongoproject.demo.domain.Post;
 import com.maneira.mongoproject.demo.domain.User;
+import com.maneira.mongoproject.demo.dto.CommentDTO;
 import com.maneira.mongoproject.demo.repository.PostRepository;
 import com.maneira.mongoproject.demo.repository.UserRepository;
 import com.maneira.mongoproject.demo.dto.AuthorDTO;
@@ -42,6 +43,10 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
---
+++ src/main/java/com/maneira/mongoproject/demo/service/PostService.java
@@ -0,0 +1,23 @@
+package com.maneira.mongoproject.demo.service;
+
+import com.maneira.mongoproject.demo.domain.Post;
+import com.maneira.mongoproject.demo.repository.PostRepository;
+import com.maneira.mongoproject.demo.service.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
